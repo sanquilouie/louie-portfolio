d23c6ec2369cace02e749950233c47b91c735ea0
louieeeee <louiesanqui0518@gmail.com>
2025-05-17 17:13:16 +0800

    test

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "Loading...";
+  const fullText = "Loafsdfsdf";
 
   useEffect(() => {
     let index = 0;
